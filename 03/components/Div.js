036e53863d9d6ba220b2c2a4bdbea351c1ce1fad
telusmikolaj <mikolaj.telus@gmail.com>
2020-02-20 13:34:56 +0100

    4th exercise

--- 03/components/Div.js
+++ 03/components/Div.js
@@ -3,7 +3,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { TextContext } from './../context';
 
-const Div = () => {
+const Div = props => {
     const { title } = props;
     const { Provider } = TextContext;
     return (
--- 04/SpeedTest.js
+++ 04/SpeedTest.js
@@ -1,17 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import useRandomItem from './hook';
 
 const SpeedTest = () => {
     const [word, regenerateWord] = useRandomItem(['devmentor.pl', 'abc', 'JavaScript']);
+    const [time, setTime] = useState(0);
+    const [text, setText] = useState('');
+    const [numOfChars, setNumOfChars] = useState(0);
+    const timerRef = useRef(null);
 
     useEffect(() => {
         regenerateWord();
     }, []);
+    useEffect(() => {
+        word.localCompare(text) ? setNumOfChars(numOfChars + 1) : '';
+    }, [text]);
+    const incrementTime = () => {
+        setTime(time => time + 1);
+    };
+    const startTimer = () => {
+        timerRef.current = setInterval(incrementTime, 1000);
+    };
+    const stopTimer = () => {
+        clearInterval(timerRef.current);
+    };
+    const handleChange = e => {
+        setText(e.target.value);
+    };
 
     return (
         <div>
             <h1>{word}</h1>
-            <input />
+            <input value={text} onFocus={startTimer} onBlur={stopTimer} onChange={handleChange} />
         </div>
     );
 };
