ebfae267dee9951a28c8c4bd4dff1e76ec76e401
Mikołaj Telus <telusmikolaj@gmail.com>
2020-02-20 15:35:17 +0100

    4th exercise complete

--- 04/SpeedTest.js
+++ 04/SpeedTest.js
@@ -6,14 +6,13 @@ const SpeedTest = () => {
     const [time, setTime] = useState(0);
     const [text, setText] = useState('');
     const [numOfChars, setNumOfChars] = useState(0);
+    const [score, setScore] = useState(0);
+    const [totalTime, setTotalTime] = useState(0);
     const timerRef = useRef(null);
 
     useEffect(() => {
         regenerateWord();
     }, []);
-    useEffect(() => {
-        word.localCompare(text) ? setNumOfChars(numOfChars + 1) : '';
-    }, [text]);
     const incrementTime = () => {
         setTime(time => time + 1);
     };
@@ -23,14 +22,32 @@ const SpeedTest = () => {
     const stopTimer = () => {
         clearInterval(timerRef.current);
     };
-    const handleChange = e => {
-        setText(e.target.value);
-    };
+  
+    useEffect(() => {
+       if(word != null){
+           if(word === text){
+               setNumOfChars(numOfChars + text.length);
+               setText('');
+               setScore(Math.round(score + 1 + (numOfChars/time)));
+               setTotalTime(totalTime + time);
+               setTime(0);
+               
+           }
+       }
+     }, [text]);
+
+     useEffect(() => {
+        regenerateWord();
+    }, [numOfChars]);
+     
 
     return (
         <div>
             <h1>{word}</h1>
-            <input value={text} onFocus={startTimer} onBlur={stopTimer} onChange={handleChange} />
+            <input value={text} onFocus={startTimer} onBlur={stopTimer} onChange={e=>setText(e.target.value)} />
+            <h5>score: {score}</h5>
+            <h5>time: {time}</h5>
+            <h5>Total Time: {totalTime}</h5>
         </div>
     );
 };
