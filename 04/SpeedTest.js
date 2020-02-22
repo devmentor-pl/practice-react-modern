c6af97a02c1ea4d50b228ff9f7f7f3350fb8d562
telusmikolaj <mikolaj.telus@gmail.com>
2020-02-22 14:55:53 +0100

    5th exercise

--- 04/SpeedTest.js
+++ 04/SpeedTest.js
@@ -22,29 +22,32 @@ const SpeedTest = () => {
     const stopTimer = () => {
         clearInterval(timerRef.current);
     };
-  
-    useEffect(() => {
-       if(word != null){
-           if(word === text){
-               setNumOfChars(numOfChars + text.length);
-               setText('');
-               setScore(Math.round(score + 1 + (numOfChars/time)));
-               setTotalTime(totalTime + time);
-               setTime(0);
-               
-           }
-       }
-     }, [text]);
 
-     useEffect(() => {
+    useEffect(() => {
+        if (word != null) {
+            if (word === text) {
+                setNumOfChars(numOfChars + text.length);
+                setText('');
+                setScore(Math.round(score + 1 + numOfChars / time));
+                setTotalTime(totalTime + time);
+                setTime(0);
+            }
+        }
+    }, [text]);
+
+    useEffect(() => {
         regenerateWord();
     }, [numOfChars]);
-     
 
     return (
         <div>
             <h1>{word}</h1>
-            <input value={text} onFocus={startTimer} onBlur={stopTimer} onChange={e=>setText(e.target.value)} />
+            <input
+                value={text}
+                onFocus={startTimer}
+                onBlur={stopTimer}
+                onChange={e => setText(e.target.value)}
+            />
             <h5>score: {score}</h5>
             <h5>time: {time}</h5>
             <h5>Total Time: {totalTime}</h5>
