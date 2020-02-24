178d829015ccacf87f8f18d4f8460fbefe60b900
telusmikolaj <mikolaj.telus@gmail.com>
2020-02-24 08:11:31 +0100

    fix

--- 03/components/Box.js
+++ 03/components/Box.js
@@ -1,20 +1,9 @@
 // ./src/components/Box.js
 import React from 'react';
-import PropTypes from 'prop-types';
 import Div from './Div';
-import { TextContext } from '../context';
 
 const Box = () => {
-    const { Provider } = TextContext;
-    return (
-        <Provider value="sibling">
-            <Div />
-        </Provider>
-    );
-};
-
-Box.propTypes = {
-    text: PropTypes.string.isRequired,
+    return <Div />;
 };
 
 export default Box;
--- 03/components/Div.js
+++ 03/components/Div.js
@@ -1,38 +1,16 @@
 // ./src/components/Div.js
 import React from 'react';
-import PropTypes from 'prop-types';
 import { TextContext, ColorContext } from '../context';
 
 const Div = () => {
     const { Consumer: TextConsumer } = TextContext;
     const { Consumer: ColorConsumer } = ColorContext;
+
     return (
         <TextConsumer>
-            {text => (
-                <ColorConsumer>
-                    {color => (
-                        <div>
-                            <h1>{text}</h1>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-                                lacinia, justo et posuere viverra, ligula turpis egestas nisi, id
-                                iaculis augue ex non nisi. Class aptent taciti sociosqu ad litora
-                                torquent per conubia nostra, pedr inceptos himenaeos. Etiam at ex
-                                orci. Praesent hendrerit diam quis tempor cursus. Aenean vitae
-                                porttitor purj us, eget luctus nisi. Mauris ex nibh, aliquet
-                                venenatis sagittis vitae, lobortis vitae ex. Donec congue dui leo,
-                                at auctor est laoreet gravida.
-                            </p>
-                        </div>
-                    )}
-                </ColorConsumer>
-            )}
+            {text => <ColorConsumer>{color => <div style={{ color }}>{text}</div>}</ColorConsumer>}
         </TextConsumer>
     );
-};
-
-Div.propTypes = {
-    title: PropTypes.string.isRequired,
 };
 
 export default Div;
