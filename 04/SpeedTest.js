8e77abd86b2cbe3866ddcbec2f10f1d87809b2f9
Bartosz Białecki <bartosz.bialecki@gmail.com>
2020-12-02 20:59:17 +0100

    Finish task 4

--- 04/SpeedTest.js
+++ 04/SpeedTest.js
@@ -1,17 +1,62 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import useRandomItem from './hook';
 
 const SpeedTest = () => {
     const [word, regenerateWord] = useRandomItem(['devmentor.pl', 'abc', 'JavaScript']);
+    const [text, setText] = useState('');
+    const [seconds, setSeconds] = useState(0);
+    const [doneWords, setDoneWords] = useState([]);
+    const intervalRef = useRef(null);
+
+    const showTime = sec => {
+        return new Date(sec * 1000).toISOString().substr(11, 8);
+    };
+
+    const stopInterval = () => {
+        if (intervalRef !== null) {
+            setSeconds(0);
+            clearInterval(intervalRef.current);
+        }
+    };
+
+    const startInterval = () => {
+        intervalRef.current = setInterval(() => {
+            setSeconds(value => value + 1);
+        }, 1000);
+    };
 
     useEffect(() => {
         regenerateWord();
     }, []);
 
+    useEffect(() => {
+        if (text === word) {
+            setDoneWords(words => [...words, { word, seconds }]);
+            setSeconds(0);
+            setText('');
+
+            regenerateWord();
+        }
+    }, [text]);
+
     return (
         <div>
             <h1>{word}</h1>
-            <input />
+            <input
+                value={text}
+                onChange={e => setText(e.target.value)}
+                onFocus={() => startInterval()}
+                onBlur={() => stopInterval()}
+            />
+            <p>Time: {showTime(seconds)}</p>
+            <h2>Results:</h2>
+            <ul>
+                {doneWords.map(item => (
+                    <li key={`${item.text}${item.seconds}`}>
+                        {item.word} - {showTime(item.seconds)}
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 };
