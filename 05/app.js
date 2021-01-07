aeb9dc07b777b9a8d7e11fa68b1976ab1bda1bda
AnnaBuller <aniabela@wp.pl>
2021-01-07 14:20:43 +0100

    add FormFields

--- 05/app.js
+++ 05/app.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import ContactForm from './ContactForm';
+import ContactForm from './components/ContactForm';
 
 const App = () => {
-    return <ContactForm />;
+    const formFields = [
+        { label: 'First Name', name: 'firstName', field: 'input', type: 'text', required: true},
+        { label: 'Last Name', name: 'lastName',  field: 'input', type: 'text', required: true},
+        { label: 'E-mail', name: 'email',  field: 'input', type: 'email', required: true},
+        { label: 'Phone Number', name: 'phone',  field: 'input', type: 'tel', required: false},
+        { label: 'Title', name: 'title',  field: 'input', type: 'text', required: true},
+        { label: 'Message', name: 'message', field: 'textarea' , required: true}
+    ]
+    return <ContactForm formFields={formFields}/>;
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
