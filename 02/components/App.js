a224a50c6f6211a14b4edd51ade118c45d4d7381
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2021-01-16 12:03:57 +0100

    Task 02 finish

--- 02/components/App.js
+++ 02/components/App.js
@@ -2,15 +2,22 @@
 import React from 'react';
 import Box from './Box';
 
+import titleContext from '../context';
+
 class App extends React.Component {
     state = {
         text: 'React HelloWorld Modern!',
     };
 
     render() {
+        const { Provider } = titleContext;
         const { text } = this.state;
 
-        return <Box text={text} />;
+        return (
+            <Provider value={text}>
+                <Box />
+            </Provider>
+        );
     }
 }
 
