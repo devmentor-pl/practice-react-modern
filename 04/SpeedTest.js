b7d984b17917ec5e0011b38c1aee17096cef6f18
Piotr Niemyjski <piotr.niemyjski@wp.pl>
2021-01-17 16:26:14 +0100

    incomplede version

--- 04/SpeedTest.js
+++ 04/SpeedTest.js
@@ -1,17 +1,52 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import useRandomItem from './hook';
+
+import DisplayResults from './DisplayResults'
 
 const SpeedTest = () => {
     const [word, regenerateWord] = useRandomItem(['devmentor.pl', 'abc', 'JavaScript']);
+    const [enteredText, setEnteredText] = useState('');
+    const [seconds, setSeconds] = useState(0);
+    const [results, setResults] = useState([])
+
+    const clockRef = useRef(null);
+    const inputRef = useRef(null);
+
+    const startClock = () => {
+        clockRef.current = setInterval(() => {
+            setSeconds(value => value + 1);
+        }, 1000);
+    }
+
+    const stopClock = () => {
+        clearInterval( clockRef.current );
+    }
 
     useEffect(() => {
         regenerateWord();
     }, []);
 
+    useEffect(() => {
+        if(enteredText === word) {
+            stopClock();
+            clockRef.current = null
+            setResults([...results, {word, seconds}])
+            regenerateWord();
+            setSeconds(0);
+            setEnteredText('')
+            inputRef.current.blur();
+        }
+    }, [enteredText])
+
     return (
         <div>
             <h1>{word}</h1>
-            <input />
+            <input  ref={ inputRef }
+                value={ enteredText }
+                onChange={ e => setEnteredText(e.target.value) }
+                onFocus={ startClock }
+                onBlur={ stopClock } />
+            <DisplayResults results={ results } />
         </div>
     );
 };
