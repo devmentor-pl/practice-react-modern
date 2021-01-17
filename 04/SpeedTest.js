523ff34b036554686668ee660b3b90010083f866
Martyna Kiljan <kmartynak@icloud.com>
2021-01-17 16:31:40 +0100

    task 04 finish

--- 04/SpeedTest.js
+++ 04/SpeedTest.js
@@ -2,13 +2,26 @@ import React, { useEffect, useRef, useState }from 'react';
 import useRandomItem from './hook';
 
 const SpeedTest = () => {
-    const [word, regenerateWord] = useRandomItem(['devmentor.pl', 'abc', 'JavaScript']);
+    const [word, regenerateWord] = useRandomItem(['chrząszcz', 'niezidentyfikowany', 'papcie', 'paralotnia', 'tymoteusz', 'obiekt', 'paprocie', 'gżegżółka', 'bobyśmy' ]);
     const [counter, setCounter] = useState(0);
-
-
+    const [valueInput, setText] = useState('');
+    const [wordCount, setWordCount] = useState(0);
+  
     const inputRef = useRef(null)
 
    
+
+    useEffect(() => {
+        if(word != null){
+            const wordLength = word.length
+            if(word === valueInput) {
+                setWordCount(wordCount + wordLength );
+                setText('')
+                regenerateWord()
+            }
+        }
+    }, [valueInput]);
+
 
     useEffect(() => {
         regenerateWord();
@@ -16,28 +29,42 @@ const SpeedTest = () => {
     }, []);
 
 
-
     const startTime = () => {
         inputRef.current = setInterval(() => {
-            setCounter(value => value + 1);
-            // regenerateWord()
+            setCounter(value => value + 1)   
+            // (jak zrobic warunek, że tylko do 1 minuty ma odliczać ?)  
+            // if(value === 60) {
+            //     console.log('finish, max 60 sec ')
+            // }   
         }, 1000);
+        
+  
     };
+
+
     const stopTime = () => {
         clearInterval(inputRef.current);
+        regenerateWord()
+        inputRef.current = ''
     }
+
+  
 
     return (
         <div>
+    
             <h1>{word}</h1>
             <input 
                 ref = {inputRef}
                 onFocus={() => startTime()}
                 onBlur={() => stopTime()}
-            
+                onChange={e=>setText(e.target.value)}
+                value={valueInput}
             />
-            <p>Time: {counter} </p>
-            <p>Number of correct letters:</p>
+         
+            <p>Number of correct letters: <span style={{color: "red"}}>{wordCount}</span>  in <span style={{color: "red"}}>{counter} sec</span> </p>
+          
+    
         </div>
     );
 };
