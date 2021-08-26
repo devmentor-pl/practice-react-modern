130328f2e5ec832fea77c9b4d213e2c9b12f8f89
Laura Zacharuk <laura.zacharuk@gmail.com>
2021-08-26 21:40:30 +0200

    task 4

--- 04/SpeedTest.js
+++ 04/SpeedTest.js
@@ -1,17 +1,64 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import useRandomItem from './hook';
 
 const SpeedTest = () => {
-    const [word, regenerateWord] = useRandomItem(['devmentor.pl', 'abc', 'JavaScript']);
+    const [word, regenerateWord] = useRandomItem(['devmentor.pl', 'abc', 'JavaScript', 'Szpiglas', 'Rzepiska', 'Bieszczady', 'Lutowiska']);
+
+    const [counter, setCounter] = useState(0);
+    const [textInput, setText] = useState('');
+    const [wordCount, setWordCount] = useState(0);
+
+    const inputRef = useRef(null);
+
+    useEffect(() => {
+        if(word != null) {
+            const wordLength = word.length 
+            if(word === textInput) {
+                setWordCount(wordCount + wordLength);
+                setText('')
+                regenerateWord()
+            }
+        }
+    }, [textInput])
 
     useEffect(() => {
         regenerateWord();
+        return () => clearInterval(inputRef.current)
     }, []);
+
+
+    const startTime = () => {
+        inputRef.current = setInterval(() => {
+
+            setCounter((value) => {
+                const nextValue = value + 1;
+                if(nextValue === 60) {
+                    stopTime();
+                }
+                return nextValue;
+            });
+        }, 1000);
+    };
+
+
+    const stopTime = () => {
+        clearInterval(inputRef.current);
+        regenerateWord()
+        inputRef.current = ''
+    }
 
     return (
         <div>
             <h1>{word}</h1>
-            <input />
+            <input 
+                ref = {inputRef} 
+                onFocus={() => startTime()}
+                onBlur={() => stopTime()}
+                onChange={e=>setText(e.target.value)}
+                value={textInput}   
+            />
+
+            <p>Number of correct letters: <span style={{color: "red"}}>{wordCount}</span> in <span style={{color: "red"}}>{counter} sec</span> </p>
         </div>
     );
 };
