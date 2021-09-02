3af45a2faf7d1c103d0e727ba76030020b956ca3
Radosław Kawczyński <kawczynski.radoslaw@gmail.com>
2021-09-02 15:33:02 +0200

    Task 03 September 2
    Task 03 September 2

--- 03/components/App.js
+++ 03/components/App.js
@@ -2,12 +2,19 @@
 import React from 'react';
 import Box from './Box';
 import Div from './Div';
+import {TextContext, ColorContext} from '../context';
 
 const App = () => {
+    const {Provider: TextProvider} = TextContext;
+    const {Provider: ColorProvider} = ColorContext;
     return (
         <section>
-            <Box />
-            <Div />
+            <Box/>
+            <TextProvider value='sibling'>
+                <ColorProvider value='blue'>
+                    <Div/>
+                </ColorProvider>
+            </TextProvider>
         </section>
     );
 };
