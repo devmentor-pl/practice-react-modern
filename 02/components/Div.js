36c8776a6e5e2140200cee353985cd3833e0ef2b
Kowacka Malgorzata <kowackag@gmail.com>
2021-10-22 00:10:42 +0200

    Finish Task 02

--- 02/components/Div.js
+++ 02/components/Div.js
@@ -1,13 +1,11 @@
-// ./src/components/Div.js
 import React from 'react';
-import PropTypes from 'prop-types';
+import TextContext from './context'
 
-const Div = props => {
-    const { title } = props;
-
+const Div =() => {
+    const {Consumer} = TextContext;
     return (
         <div>
-            <h1>{title}</h1>
+            <Consumer>{ context => <h1>{context}</h1>}</Consumer>
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lacinia, justo et posuere viverra,
                 ligula turpis egestas nisi, id iaculis augue ex non nisi. Class aptent taciti sociosqu ad litora
@@ -15,12 +13,8 @@ const Div = props => {
                 tempor cursus. Aenean vitae porttitor purj us, eget luctus nisi. Mauris ex nibh, aliquet venenatis
                 sagittis vitae, lobortis vitae ex. Donec congue dui leo, at auctor est laoreet gravida.
             </p>
-        </div>
+        </div> 
     );
-};
-
-Div.propTypes = {
-    title: PropTypes.string.isRequired,
 };
 
 export default Div;
