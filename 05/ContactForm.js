a401ac146bac9616653a259d931bb487a84e09b9
Kowacka Malgorzata <kowackag@gmail.com>
2021-10-25 15:30:37 +0200

    Task 05

--- 05/ContactForm.js
+++ 05/ContactForm.js
@@ -1,6 +1,6 @@
 import React, {useReducer} from 'react';
-// import './ContactForm.css'
 // import account from './account';
+import emailjs from "emailjs-com";
 
 const ContactForm = () => {
     // console.log(account);
@@ -17,41 +17,34 @@ const ContactForm = () => {
     }
     const [state, dispatch] = useReducer(reducer, init);
     const {firstName, lastName, email, phone, title, message} = state;
-    const validateData = ({firstName, lastName, email, title, message}) => {
+    const validateData = () => {
         const errors = [];
         if (firstName.length<3) errors.push('Wpisz poprawnie imię');
         if (lastName.length<3) errors.push('Wpisz poprawnie nazwisko');
         if (email.length<3 || !email.includes('@')) errors.push('Wpisz poprawnie adres email');
         if (title.length<3) errors.push('Wpisz poprawnie temat wiadomości');
         if (message.length<20) errors.push('treść wiadomości powinna zawierać conajmniej 20 znaków');
-        if (errors.length >0 ){
+        if (errors.length>0) {
             alert(errors);
-            return false;
-        } else {return true}
+            return false
+        } else { 
+            return true}
     }
     const handleForm = (e) => {
         e.preventDefault();
-        if (validateData(state)) {
-            console.log('wysłane')
+        if (validateData()) {
             console.log(state);
-            // dispatch(state=init)
-            // useReducer(reducer, init);
-            // const reducer = (state, init) => {
-            //     return {init};
-            // }
         }
     }
 
-    // console.log(firstName);
-    return (<form className="form" onSubmit={(e)=> handleForm(e)}>
-        <label className="label">Imię<input name="firstName" value={firstName} onChange={ e => dispatch(e.target)}/></label>
-        <label className="label">Nazwisko<input name="lastName" value={lastName} onChange={ e => dispatch(e.target)}/></label>
-        <label className="label"> Adres email<input name="email" value={email} onChange={ e => dispatch(e.target)}/></label>
-        <label className="label"> Telefon<input name="phone" value={phone}  onChange={ e => dispatch(e.target)}/></label>
-        <label className="label"> Temat<input name="title" value={title} onChange={ e => dispatch(e.target)}/></label>
-        <label className="label">Treść wiadomości<input name="message" value={message} onChange={ e => dispatch(e.target)}/></label>
-        <input type="submit"/>
-        
+    return (<form onSubmit={(e)=> handleForm(e)}>
+        <label htmlFor="firstName">Imię<input name="firstName" value={firstName} onChange={ e => dispatch(e.target)}/></label>
+        <label htmlFor ="lastName">Nazwisko<input name="lastName" value={lastName} onChange={ e => dispatch(e.target)}/></label>
+        <label htmlFor ="email"> Adres email<input name="email" value={email} onChange={ e => dispatch(e.target)}/></label>
+        <label htmlFor="phone"> Telefon<input name="phone" value={phone}  onChange={ e => dispatch(e.target)}/></label>
+        <label htmlFor="title"> Temat<input name="title" value={title} onChange={ e => dispatch(e.target)}/></label>
+        <label htmlFor="message">Treść wiadomości<input name="message" value={message} onChange={ e => dispatch(e.target)}/></label>
+        <input type="submit"/>    
     </form>);
 };
 
