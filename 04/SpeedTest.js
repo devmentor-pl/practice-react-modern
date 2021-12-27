02311456d12157dd3d579d2a1f06f5652cca77fa
Jakub Chrobak <jchrobakk@gmail.com>
2021-12-27 14:38:10 +0100

    Timer bug

--- 04/SpeedTest.js
+++ 04/SpeedTest.js
@@ -1,17 +1,45 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import useRandomItem from './hook';
 
 const SpeedTest = function () {
-    const [word, regenerateWord] = useRandomItem(['devmentor.pl', 'abc', 'JavaScript']);
+    const [word, regenerateWord] = useRandomItem(['abc']);
+    const [inputValue, setInputValue] = useState('');
+    const [timer, setTimer] = useState(0);
+    const intervalRef = useRef(null);
 
     useEffect(() => {
         regenerateWord();
     }, []);
 
+    const compareWords = () => {
+        console.log('compare', inputValue, word);
+        if (inputValue === word) {
+            setInputValue('');
+            regenerateWord();
+            console.log('correct');
+        }
+    };
+
+    const onChange = (e) => {
+        setInputValue(e.target.value);
+        compareWords();
+    };
+
+    const startTimer = () => {
+        intervalRef.current = setInterval(() => {
+            setTimer(timer + 1);
+        }, 1000);
+    };
+
+    const stopTimer = () => {
+        clearInterval(intervalRef.current);
+    };
+
     return (
         <div>
             <h1>{word}</h1>
-            <input />
+            <input onBlur={stopTimer} onFocus={startTimer} value={inputValue} onChange={onChange} />
+            <p>{timer}</p>
         </div>
     );
 };
