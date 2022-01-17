a0af49213742d67c173f79ea79dad2fda03b7a29
Dawid Krawczyk <dawidosss1998@gmail.com>
2022-01-17 11:43:48 +0100

    Fourth task

--- 04/SpeedTest.js
+++ 04/SpeedTest.js
@@ -1,17 +1,49 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import useRandomItem from './hook';
 
 function SpeedTest() {
     const [word, regenerateWord] = useRandomItem(['devmentor.pl', 'abc', 'JavaScript']);
+    const [inputValue, setValue] = useState('');
+    const [counter, setCounter] = useState(0);
+    const [wordsLength, setWordsLength] = useState(0);
+    const intervalRef = useRef(null);
 
     useEffect(() => {
         regenerateWord();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
+
+    const stopTimer = () => {
+        clearInterval(intervalRef.current);
+    };
+
+    const startTimer = () => {
+        intervalRef.current = setInterval(() => {
+            setCounter((value) => value + 1);
+        }, 1000);
+        return () => stopTimer();
+    };
+
+    const onChange = (e) => {
+        setValue(e.target.value);
+    };
+
+    useEffect(() => {
+        if (inputValue === word) {
+            setWordsLength(wordsLength + word.length);
+            setValue('');
+            regenerateWord();
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [inputValue]);
 
     return (
         <div>
-            <h1>{word}</h1>
-            <input />
+            <h1>Your word is: {word}</h1>
+            <input value={inputValue} onChange={onChange} onFocus={startTimer} onBlur={stopTimer} />
+            <span>
+                You wrote: {wordsLength} letters in {counter} sec
+            </span>
         </div>
     );
 }
