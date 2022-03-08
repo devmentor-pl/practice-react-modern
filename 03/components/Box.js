be7511d22e5ee52d53da30a7527ad13e9f2d7dd5
Kajetan Kisielewski <kajetankisielewski@gmail.com>
2022-03-08 16:35:50 +0100

    Task 03 - done

--- 03/components/Box.js
+++ 03/components/Box.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import Div from './Div';
 
+
 function Box() {
     return <Div />;
 }
--- 03/components/Div.js
+++ 03/components/Div.js
@@ -1,18 +1,33 @@
 // ./src/components/Div.js
 import React from 'react';
+import { TextContext, ColorContext } from '../context';
 
 function Div() {
+    const { Consumer: TextConsumer } = TextContext;
+    const { Consumer: ColorConsumer } = ColorContext;
+
     return (
-        <div>
-            <h1>??</h1>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lacinia, justo et posuere viverra,
-                ligula turpis egestas nisi, id iaculis augue ex non nisi. Class aptent taciti sociosqu ad litora
-                torquent per conubia nostra, pedr inceptos himenaeos. Etiam at ex orci. Praesent hendrerit diam quis
-                tempor cursus. Aenean vitae porttitor purj us, eget luctus nisi. Mauris ex nibh, aliquet venenatis
-                sagittis vitae, lobortis vitae ex. Donec congue dui leo, at auctor est laoreet gravida.
-            </p>
-        </div>
+        <TextConsumer>
+            {(text) => (
+                <ColorConsumer>
+                    {(color) => (
+                        <div style={{ border: `1px solid ${color}` }}>
+                            <h1>{text}</h1>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+                                lacinia, justo et posuere viverra, ligula turpis egestas nisi, id
+                                iaculis augue ex non nisi. Class aptent taciti sociosqu ad litora
+                                torquent per conubia nostra, pedr inceptos himenaeos. Etiam at ex
+                                orci. Praesent hendrerit diam quis tempor cursus. Aenean vitae
+                                porttitor purj us, eget luctus nisi. Mauris ex nibh, aliquet
+                                venenatis sagittis vitae, lobortis vitae ex. Donec congue dui leo,
+                                at auctor est laoreet gravida.
+                            </p>
+                        </div>
+                    )}
+                </ColorConsumer>
+            )}
+        </TextConsumer>
     );
 }
 
