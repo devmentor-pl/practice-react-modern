269c3d642e793d47078e9cf3ad5cf105d4c2f2a8
Ewelina <ewelina@ekopacz.pl>
2022-03-30 22:36:40 +0200

    upgrade task 01

--- 01/components/App.js
+++ 01/components/App.js
@@ -3,9 +3,12 @@ import React from 'react';
 import Clock from './Clock';
 
 class App extends React.Component {
-    state = { // został mi 1 błąd: State initialization should be in a constructor, mam dodany do .eslintrc  "parser": "@babel/eslint-parser", i zasintalowany: npm i @babel/eslint-parser@7 -D
-        date: new Date(),
-    };
+    constructor() {
+        super();
+        this.state = {
+            date: new Date()
+        }
+    }
 
     componentDidMount() {
         this.intervalId = setInterval(() => {
