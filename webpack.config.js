c5a5387b8a19afddfdf149ee242e301e81499f23
Anna <krolikowska.ann@gmail.com>
2022-06-18 14:10:58 +0200

    added eslint, prettier

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,9 @@ const taskNumber = "01";
 const path = require("path");
 // importuję bibliotękę [path] z [node.js]
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ESLintPlugin = require('eslint-webpack-plugin')
 // importuję odpowiedni plugin
+
 module.exports = {
   entry: `./${taskNumber}/app.js`,
   // definiuje plik wejściowy
@@ -37,7 +39,8 @@ module.exports = {
       // wskazuje plik źródłowy
       filename: "index.html"
       // określan nazwę dla pliku
-    })
+    }),
+    new ESLintPlugin()
   ]
 };
 // eksportuję ustawienia dla webpack-a
