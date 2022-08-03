b31013390b745b065ac1fe112d0c59b85ae5ef98
Andrzej <andis_as@wp.pl>
2022-08-03 20:20:31 +0200

    zad 5 - form validation

--- 05/ContactForm.js
+++ 05/ContactForm.js
@@ -18,61 +18,143 @@ function ContactForm() {
     const subject = useChangeHandler()
     const news = useChangeHandler()
 
-    // const divFormName = {display: 'inline-block', width: '80px'}
+    const divFormWrapper = { padding: '5px' }
+    const divFormName = { display: 'inline-block', width: '80px' }
+    const divError = { color: 'red' }
+    const divFormInput = { display: 'inline-block' }
+
+    // eslint-disable-next-line no-unused-vars
+    const [errors, setErrors] = React.useState({'a':10})
 
     const onSubmit = e => {
         e.preventDefault()
         console.log('submit')
+        setErrors({})
+        console.log( errors )
+        const firstNameField = firstName.value
+        if (firstNameField.length < 3) {
+            // setErrors([...errors, {'firstName':'Enter at least 3 chars'}])     // Array field
+            // setErrors(val => [...val, {'firstName':'Enter at least 3 chars'}]) // Array ok
+            setErrors(val => ({ ...val, firstName: 'Enter at least 3 chars' }))
+        }
+        const lastNameField = lastName.value
+        if (lastNameField.length < 3) {
+            // setErrors(val => [...val, {'lastName':'Enter at least 3 chars'}])
+            setErrors(val => ({ ...val, lastName: 'Enter at least 3 chars' }))
+        }
+        const emailField = email.value
+        // eslint-disable-next-line no-useless-escape
+        const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        const checkEmail = emailField.match(mailformat)
+        if(!checkEmail) {
+            setErrors(val => ({ ...val, email: 'Email is not good' }))
+        }
+        const phoneField = phone.value
+        if (phoneField.length < 3) {
+            setErrors(val => ({ ...val, phone: 'Enter at least 3 chars' }))
+        } else if (Number.isNaN(Number(phoneField))) {
+            console.log('Not a number')
+            setErrors(val => ({ ...val, phone: 'Not a number' }))
+        }
+        const subjectField = subject.value
+        if (subjectField.length < 3) {
+            setErrors(val => ({ ...val, subject: 'Enter at least 3 chars' }))
+        }
+        const newsField = news.value
+        if (newsField.length < 3) {
+            setErrors(val => ({ ...val, news: 'Enter at least 3 chars' }))
+        }
+
+        // const test = Object.keys(errors)
+        // console.log( test )
+        // if(test.length === 0) {
+        //     console.log('Form sending ....')
+        // } else {
+        //     console.log('Form NOT send')
+        // }
     }
+
+    React.useEffect(() => {
+        const test = Object.keys(errors)
+        console.log( test )
+        if(test.length === 0) {
+            console.log('Form sending ....')
+        } else {
+            console.log('Form NOT send')
+        }
+    }, [errors])
+
     return (
         <form onSubmit={onSubmit}>
-            <div>
+            <div style={divFormWrapper}>
                 <label htmlFor="name">
-                    <div style={{display: 'inline-block', width: '80px'}}>
-                        name 
+                    <div style={divFormName}>
+                        name
                     </div>
-                    <input name="firstName" {...firstName} id='firstName' />
+                    <div style={divFormInput}>
+                        <div style={divError}>{errors.firstName}</div>
+                        <input name="firstName" {...firstName} id='firstName' />
+                    </div>
                 </label>
             </div>
-            <div>
+            <div style={divFormWrapper}>
                 <label htmlFor="lastName">
-                    <div style={{display: 'inline-block', width: '80px'}}>
+                    <div style={divFormName}>
                         last
                     </div>
-                    <input name="lastName" {...lastName} id='lastName' />
+                    <div style={divFormInput}>
+                        <div style={divError}>{errors.lastName}</div>
+                        <input name="lastName" {...lastName} id='lastName' />
+                    </div>
                 </label>
             </div>
-            <div>
+            <div style={divFormWrapper}>
                 <label htmlFor="email">
-                    <div style={{display: 'inline-block', width: '80px'}}>
+                    <div style={divFormName}>
                         email
                     </div>
-                    <input name="email" {...email} />
+                    <div style={divFormInput}>
+                        <div style={divError}>{errors.email}</div>
+                        <input name="email" {...email} />
+                    </div>
                 </label>
             </div>
-            <div>
+            <div style={divFormWrapper}>
                 <label htmlFor="phone">
-                    <div style={{display: 'inline-block', width: '80px'}}>
+                    <div style={divFormName}>
                         phone
                     </div>
-                    <input name="phone" {...phone} />
+                    <div style={divFormInput}>
+                        <div style={divError}>{errors.phone}</div>
+                        <input name="phone" {...phone} />
+                    </div>
                 </label>
             </div>
-            <div>
+            <div style={divFormWrapper}>
                 <label htmlFor="subject">
-                    <div style={{display: 'inline-block', width: '80px'}}>
+                    <div style={divFormName}>
                         subject
                     </div>
-                    <input name="subject" {...subject} />
+                    <div style={divFormInput}>
+                        <div style={divError}>{errors.subject}</div>
+                        <input name="subject" {...subject} />
+                    </div>
                 </label>
             </div>
-            <div>
+            <div style={divFormWrapper}>
                 <label htmlFor="news">
-                    <div style={{display: 'inline-block', width: '80px'}}>
+                    <div style={divFormName}>
                         news
                     </div>
-                    <input name="news" {...news} />
+                    <div style={divFormInput}>
+                        <div style={divError}>{errors.news}</div>
+                        <input name="news" {...news} />
+                    </div>
                 </label>
+            </div>
+            <div style={divFormWrapper}>
+                <div style={divFormName} />
+                <button type='submit'>SEND</button>
             </div>
         </form>
     )
