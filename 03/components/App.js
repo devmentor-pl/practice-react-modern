7fd1349e61fd0cfde8d444193b63cb6e8f75d22c
Filip <43039909+Urfisior@users.noreply.github.com>
2022-08-27 23:27:24 +0100

    task 03 done

--- 03/components/App.js
+++ 03/components/App.js
@@ -3,11 +3,16 @@ import React from 'react';
 import Box from './Box';
 import Div from './Div';
 
+import { TextContext } from '../context';
+
 const App = () => {
+    const { Provider } = TextContext;
     return (
         <section>
             <Box />
-            <Div />
+            <Provider value="sibling">
+                <Div />
+            </Provider>
         </section>
     );
 };
