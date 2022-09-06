bb085954f8771d27b158d4161d9ca7d54065970e
Mateusz Bogolubow <mateusz@bogolubow.com>
2022-09-06 19:50:05 +0200

    Finish 01

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const taskNumber = "01";
 // numer przerabianego zadania
 
+const ESLintPlugin = require('eslint-webpack-plugin');
+
 const path = require("path");
 // importuję bibliotękę [path] z [node.js]
 const HtmlWebpackPlugin = require("html-webpack-plugin");
@@ -40,7 +42,8 @@ module.exports = {
       // wskazuje plik źródłowy
       filename: "index.html"
       // określan nazwę dla pliku
-    })
+    }),
+    new ESLintPlugin()
   ]
 };
 // eksportuję ustawienia dla webpack-a
