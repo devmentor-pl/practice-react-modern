faa9bb0adf9ee30ec3555fe36f1b05c488ce6a33
Camila Montes Celinski <camicelinski@gmail.com>
2022-09-16 22:34:38 +0200

    Finish task 01

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
-const taskNumber = "01";
+const taskNumber = '01';
 // numer przerabianego zadania
 
-const path = require("path");
+const path = require('path');
 // importuję bibliotękę [path] z [node.js]
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 // importuję odpowiedni plugin
 module.exports = {
   entry: `./${taskNumber}/app.js`,
@@ -11,12 +12,12 @@ module.exports = {
   mode: 'development',
     // definiuję tryb działania
   output: {
-    path: path.resolve(__dirname, "build"),
-    // definiuje ścieżką wyjściową
-    filename: "app.min.js"
+      path: path.resolve(__dirname, 'build'),
+        // definiuje ścieżką wyjściową
+    filename: 'app.min.js',
     // definiuję nazwę pliku wyjściowego
   },
-  target: "web", 
+  target: 'web',
     // niezbędne do uruchomienia automatycznego odświeżania z webpack-dev-server@3
     // https://github.com/webpack/webpack-dev-server/issues/2758
   module: {
@@ -26,21 +27,22 @@ module.exports = {
         // określam jakie pliki
         // będą brane pod uwagę
         exclude: /node_modules/,
-        // określam wykluczenia
-        use: ["babel-loader"]
-        // określam jakie loader-y
-        // mają być wykorzystywane
-      }
-    ]
+                // określam wykluczenia
+            use: ['babel-loader'],
+                // określam jakie loader-y
+                // mają być wykorzystywane
+      },
+    ],
     // obecnie brak dodatkowych ustawień
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: `./${taskNumber}/index.html`,
-      // wskazuje plik źródłowy
-      filename: "index.html"
-      // określan nazwę dla pliku
-    })
-  ]
+            // wskazuje plik źródłowy
+          filename: 'index.html',
+            // określan nazwę dla pliku
+        }),
+    new ESLintPlugin(),
+  ],
 };
 // eksportuję ustawienia dla webpack-a
