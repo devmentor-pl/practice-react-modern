b75a45497596a81a31936f61e23c802de794d573
Szymon Ludwikowski <ludwikowski.szym1993@gmail.com>
2022-10-18 20:31:42 +0200

    Finish 02

--- 02/components/App.js
+++ 02/components/App.js
@@ -1,16 +1,23 @@
 // ./src/components/App.js
 import React from 'react';
+
 import Box from './Box';
+
+import TextContentex from '../context';
 
 class App extends React.Component {
     state = {
-        text: 'React HelloWorld Modern!',
+        text: 'React HelloWorld Modern! xyz',
     };
 
     render() {
         const { text } = this.state;
 
-        return <Box text={text} />;
+        return (
+            <TextContentex.Provider value={text}>
+                <Box />
+            </TextContentex.Provider>
+        );
     }
 }
 
