8e06913922ca09dee1e2d49f9d69c0aab463332f
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2022-12-24 12:46:52 +0100

    Export contexts

--- 03/components/App.js
+++ 03/components/App.js
@@ -2,14 +2,19 @@
 import React from 'react';
 import Box from './Box';
 import Div from './Div';
+// eslint-disable-next-line no-unused-vars
+import { TextContext } from '../context';
 
-const App = () => {
+function App() {
+
     return (
         <section>
             <Box />
-            <Div />
+            <TextContext.Provider value="sibling">
+                <Div/>
+            </TextContext.Provider>
         </section>
     );
-};
+}
 
 export default App;
