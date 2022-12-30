2efb28ce05299afa8f2fd5fd4ea2538c24363a28
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2022-12-30 12:57:44 +0100

    Add & render formElementsList

--- 05/ContactForm.js
+++ 05/ContactForm.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-unused-vars */
 
 import React, { useReducer} from 'react';
 import validateForm from './validator';
 import './styles.css';
+import fields from './fields';
 // import account from './account';
 
 function ContactForm() {
@@ -60,34 +62,20 @@ function ContactForm() {
             })
         }
     }
+    const formElementsList = fields.map(element =>{
+        const Tag = element.htmlTag;
+        return (
+            <label className="form__label" htmlFor={element.name}>
+                {element.label}
+                <Tag className={element.class} type={element.type} name={element.name}/>
+                {state.errors !== null && state.errors[element.name] ? <span className="form__error"style={{color: "red"}}>{state.errors[element.name]}</span> : null}
+            </label>
+        )
+    })
 
     return (
         <form className="form" ref={formRef} onSubmit={(e)=>handleSubmit(e, formRef)}>
-            <label className="form__label" htmlFor="name">
-                Imię i nazwisko:
-                <input className="form__input" type="text" name="name" />
-                {state.errors !== null && state.errors.name ? <span className="form__error"style={{color: "red"}}>{state.errors.name}</span> : null}
-            </label>
-            <label className="form__label" htmlFor="email">
-                E-mail:
-                <input className="form__input" type="text" name="email" />
-                {state.errors !== null && state.errors.email ? <span className="form__error" style={{color: "red"}}>{state.errors.email}</span> : null}
-            </label>
-            <label className="form__label" htmlFor="phone">
-                Telefon:
-                <input className="form__input" type="text" name="phone" />
-                {state.errors !== null && state.errors.phone ? <span className="form__error" style={{color: "red"}}>{state.errors.phone}</span> : null}
-            </label>
-            <label className="form__label" htmlFor="subject">
-                Temat:
-                <input className="form__input" type="text" name="subject" />
-                {state.errors !== null && state.errors.subject ? <span style={{color: "red"}}>{state.errors.subject}</span> : null}
-            </label>
-            <label className="form__label" htmlFor="message">
-                Wiadomość:
-                <textarea className="form__textarea" type="text" name="message" />
-                {state.errors !== null && state.errors.message ? <span style={{color: "red"}}>{state.errors.message}</span> : null}
-            </label>
+            {formElementsList}
             {state.success ? <span>Dziękujemy za wiadomość!</span> : null}
             <button className="form__button" type="submit">Wyślij</button>
         </form>
