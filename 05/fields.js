541e73faf5cb5751f04aee294d55a4f290963861
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2022-12-30 12:56:38 +0100

    Add htmlTag & class property

--- 05/fields.js
+++ 05/fields.js
@@ -5,6 +5,8 @@ const fields = [
         label: 'Imię i nazwisko:',
         type: 'text',
         required: 'true',
+        htmlTag: 'input',
+        class: 'form__input',
         pattern: `^[A-Z][a-z]* [A-Z][a-z]*$`
     },
     {
@@ -12,24 +14,32 @@ const fields = [
         label: 'E-mail:',
         type: 'text',
         required: 'true',
+        htmlTag: 'input',
+        class: 'form__input',
         pattern: `^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$`
     },
     {
         name: 'phone',
         label: 'Telefon:',
         type: 'text',
+        htmlTag: 'input',
+        class: 'form__input',
         pattern: `^(?:\+\d{2})?\d{9}$`
     },
     {
         name: 'subject',
         label: 'Temat:',
         type: 'text',
+        htmlTag: 'input',
+        class: 'form__input',
         required: 'true',
     },
     {
         name: 'message',
         label: 'Wiadomość:',
         type: 'text',
+        htmlTag: 'textarea',
+        class: 'form__textarea',
         required: 'true',
     },
 ]
