18c6f0ee82b5079cd7694f0acac2df50e28027c0
Jacek Zielinski <gandi34@wp.pl>
2023-01-15 14:40:51 +0100

    04 done

--- 04/SpeedTest.js
+++ 04/SpeedTest.js
@@ -1,17 +1,53 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState, } from 'react';
 import useRandomItem from './hook';
 
-const SpeedTest = () => {
+const SpeedTest = function SpeedTest() {
     const [word, regenerateWord] = useRandomItem(['devmentor.pl', 'abc', 'JavaScript']);
+    const [text, putText] = useState ('');
+    const [timing, setTiming] = useState(0);
+    const [length, putlength] = useState(0);
+
+
+    const intervalRef = useRef(null);
+
 
     useEffect(() => {
+
+
         regenerateWord();
+
     }, []);
+
+    useEffect(() => { 
+        if (text ===word) {
+            regenerateWord();
+            putlength((curr) => curr + text.length);
+            putText('');
+        }
+    }, [text])
+    
+
+
+    function startInterval() {
+        intervalRef.current = setInterval(() => {
+            setTiming((currentValue) => currentValue + 1);
+        }, 1000);
+    }
+
+    function stopInterval() {
+        clearInterval(intervalRef.current);
+    }
 
     return (
         <div>
             <h1>{word}</h1>
-            <input />
+            <h2>{timing > 0 ? length / timing:0}</h2>
+            <h3>{timing}</h3>
+            <input 
+                value={text} 
+                onBlur={stopInterval} 
+                onFocus={startInterval} 
+                onChange={(e)=>putText(e.target.value)}/>
         </div>
     );
 };
