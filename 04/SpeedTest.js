72b0a62ea1eff877807bafbdd574968c16ffed95
Anna <aniamatrackasadurek@gmail.com>
2023-03-21 11:57:28 +0100

    finish 04

--- 04/SpeedTest.js
+++ 04/SpeedTest.js
@@ -1,17 +1,46 @@
-import React, { useEffect } from 'react';
+/* eslint-disable react/function-component-definition */
+import React, { useEffect, useState, useRef } from 'react';
 import useRandomItem from './hook';
 
 const SpeedTest = () => {
     const [word, regenerateWord] = useRandomItem(['devmentor.pl', 'abc', 'JavaScript']);
+    const [time, setTime] = useState(0);
+    const [input, setInput] = useState('');
+    const [counter, setCounter] = useState(0);
+    const intervalRef = useRef(null);
 
     useEffect(() => {
         regenerateWord();
     }, []);
 
+    const runInterval = () => {
+        intervalRef.current = setInterval( () => {
+            setTime((val) => val + 1);
+        }, 1000);
+    }
+
+    const stopInterval = () => {
+        clearInterval(intervalRef.current);
+    };
+
+    useEffect( () => {
+        if (word === input) {
+            setInput('');
+            setCounter(counter + word.length);
+            regenerateWord();
+        }
+    }, [input]);
+
     return (
         <div>
-            <h1>{word}</h1>
-            <input />
+            <h1>Drawn word: {word}</h1>
+            <input 
+                value={input}
+                onChange={ (e) => setInput(e.target.value)}
+                onFocus={ () => runInterval() }
+                onBlur={ () => stopInterval() }
+            />
+            <p>Well done! You managed to write {counter} characters in {time} seconds!</p>
         </div>
     );
 };
