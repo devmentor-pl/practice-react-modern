c3967935cdc6564f0a4c337645a4abddd9cee078
Michal <maxpoz3212@gmail.com>
2023-04-25 20:19:02 +0200

    Change game logic

--- 04/SpeedTest.js
+++ 04/SpeedTest.js
@@ -5,8 +5,10 @@ function SpeedTest() {
     const [word, regenerateWord] = useRandomItem(['devmentor.pl', 'abc', 'JavaScript']);
     const [text, setText] = useState('');
     const [focused, setFocused] = useState(false);
-    const [time, setTime] = useState(0);
+    const [counter, setCounter] = useState(5);
     const [textLength, setTextLength] = useState(0);
+    const [finish, setFinish] = useState(false);
+    const [disabled, setDisabled] = useState(false);
     const intervalRef = useRef(null);
 
     const stopInterval = () => {
@@ -18,27 +20,42 @@ function SpeedTest() {
     }, []);
 
     useEffect(() => {
-        if (focused === true) {
-            const startTime = new Date();
+        if (focused === true && !finish) {
             intervalRef.current = setInterval(() => {
-                const elapsedTime = Date.now() - startTime;
-                setTime((elapsedTime / 1000).toFixed(2));
-            }, 100);
-        }
+                setCounter((currCounter) => {
+                    const nextCounter = currCounter - 1;
 
+                    if (nextCounter === 0) {
+                        stopInterval();
+                    }
+
+                    return nextCounter;
+                });
+            }, 1000);
+        }
         return () => stopInterval();
-    }, [focused]);
-    
+    }, [focused, finish]);
+
     useEffect(() => {
-        if (text === word) {
+        if (text === word && !finish) {
             setTextLength((value) => value + text.length);
             setText('');
             regenerateWord();
         }
-    }, [text]);
+    }, [text, finish]);
+
+    useEffect(() => {
+        if (counter === 0) {
+            setFinish(true);
+            setText('');
+            setDisabled(true);
+        }
+    }, [counter]);
 
     const handleClick = () => {
-        setTime(0);
+        setFinish(false);
+        setDisabled(false);
+        setCounter(5);
         setTextLength(0);
     };
 
@@ -47,14 +64,16 @@ function SpeedTest() {
             <h1>{word}</h1>
             <input
                 value={text}
+                disabled={disabled}
                 onChange={(e) => setText(e.target.value)}
                 onFocus={() => setFocused(true)}
                 onBlur={() => setFocused(false)}
             />
-            <p>Time: {time}s</p>
+            {finish && <h2>Time&apos;s up!</h2>}
+            <p>Time: {counter}s</p>
             <p>Correct letters: {textLength}</p>
             <button type="button" onClick={() => handleClick()}>
-                Reset
+                Restart
             </button>
         </div>
     );
