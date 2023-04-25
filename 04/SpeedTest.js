98913dfc70bab9f8b0be145ca65c1a00cfef338a
Michal <maxpoz3212@gmail.com>
2023-04-25 19:36:53 +0200

    Add task04 solution

--- 04/SpeedTest.js
+++ 04/SpeedTest.js
@@ -1,17 +1,53 @@
-import React, { useEffect } from 'react';
-import useRandomItem from './hook';
+import React, { useEffect, useRef, useState } from 'react';
+import useRandomItem from './hooks';
 
 function SpeedTest() {
     const [word, regenerateWord] = useRandomItem(['devmentor.pl', 'abc', 'JavaScript']);
+    const [text, setText] = useState('');
+    const [focused, setFocused] = useState(false);
+    const [time, setTime] = useState(0);
+    const [textLength, setTextLength] = useState(0);
+    const intervalRef = useRef(null);
+
+    const stopInterval = () => {
+        clearInterval(intervalRef.current);
+    };
 
     useEffect(() => {
         regenerateWord();
     }, []);
 
+    useEffect(() => {
+        if (focused === true) {
+            const startTime = new Date();
+            intervalRef.current = setInterval(() => {
+                const elapsedTime = Date.now() - startTime;
+                setTime((elapsedTime / 1000).toFixed(2));
+            }, 100);
+        }
+
+        return () => stopInterval();
+    }, [focused]);
+
+    useEffect(() => {
+        if (text === word) {
+            setTextLength((value) => value + text.length);
+            setText('');
+            regenerateWord();
+        }
+    }, [text]);
+
     return (
         <div>
             <h1>{word}</h1>
-            <input />
+            <input
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                onFocus={() => setFocused(true)}
+                onBlur={() => setFocused(false)}
+            />
+            <p>Time: {time}s</p>
+            <p>Correct letters: {textLength}</p>
         </div>
     );
 }
