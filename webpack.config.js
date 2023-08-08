37c5c756456799e53dad134ac5d775e0225450e5
Jędrzej Kmak <jedrzej.kmak@gmail.com>
2023-08-08 22:40:23 +0200

    finish task 3

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const taskNumber = "01";
+const taskNumber = "02";
 // numer przerabianego zadania
 
 const path = require("path");
@@ -6,40 +6,41 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 // importuję odpowiedni plugin
 const ESLintPlugin = require('eslint-webpack-plugin');
+
 module.exports = {
-  entry: `./${taskNumber}/app.js`,
-  // definiuje plik wejściowy
-  mode: 'development',
+    entry: `./${taskNumber}/app.js`,
+    // definiuje plik wejściowy
+    mode: 'development',
     // definiuję tryb działania
-  output: {
-    path: path.resolve(__dirname, "build"),
-    // definiuje ścieżką wyjściową
-    filename: "app.min.js"
+    output: {
+        path: path.resolve(__dirname, "build"),
+        // definiuje ścieżką wyjściową
+        filename: "app.min.js"
     // definiuję nazwę pliku wyjściowego
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        // określam jakie pliki
-        // będą brane pod uwagę
-        exclude: /node_modules/,
-        // określam wykluczenia
-        use: ["babel-loader"]
-        // określam jakie loader-y
-        // mają być wykorzystywane
-      }
-    ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                // określam jakie pliki
+                // będą brane pod uwagę
+                exclude: /node_modules/,
+                // określam wykluczenia
+                use: ["babel-loader"]
+                // określam jakie loader-y
+                // mają być wykorzystywane
+            }
+        ]
     // obecnie brak dodatkowych ustawień
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: `./${taskNumber}/index.html`,
-      // wskazuje plik źródłowy
-      filename: "index.html"
-      //
-    }),
-    new ESLintPlugin()
-  ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: `./${taskNumber}/index.html`,
+            // wskazuje plik źródłowy
+            filename: "index.html"
+            //
+        }),
+        new ESLintPlugin()
+    ]
 };
 // eksportuję ustawienia dla webpack-a
