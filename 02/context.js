97ae7b5d03579f107f5b0798598f9dc0e45d7abd
Fryderyk Jellinek <fryderyk.jellinek@gmail.com>
2023-12-05 23:01:42 +0100

    task 4 done

--- 02/context.js
+++ 02/context.js
@@ -2,4 +2,5 @@ import React from "react";
 
 
 const TextContext = React.createContext('React HelloWorld Modern!');
+
 export default TextContext;
--- 04/SpeedTest.js
+++ 04/SpeedTest.js
@@ -1,17 +1,47 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import useRandomItem from './hook';
 
 function SpeedTest() {
-    const [word, regenerateWord] = useRandomItem(['devmentor.pl', 'abc', 'JavaScript']);
+    const [word, regenerateWord] = useRandomItem(['abc']);
+    const [focused, setFocused] = useState(false);
+    const [inputValue, setInputValue] = useState('');
+    const [time, setTime] = useState(0);
+    const intervalRef = useRef(null);
+    
+    const startTimer = () => {
+        intervalRef.current = setInterval(() => {
+            setTime((prev) => prev + 1);
+        }, 1000);
+    };
+    const stopTimer = () => {
+        clearInterval(intervalRef.current);
+    }
+    const onFocusHandler = () => {
+        setFocused(!focused);
+        startTimer()
+    };
+    const onBlurHandler = () => {
+        setFocused(!focused);
+        stopTimer()
+    }
+    const handleOnChange = (e) => {
+        setInputValue(e.target.value);
+    }
 
-    useEffect(() => {
-        regenerateWord();
-    }, []);
+    useEffect(() => { 
+        regenerateWord(); 
+        if(inputValue === word && focused) {
+            console.log(inputValue.length)
+            regenerateWord()
+            setInputValue('');
+        }
+    }, [inputValue, word]);
 
     return (
         <div>
             <h1>{word}</h1>
-            <input />
+            {time < 10 ? <h2>0{time}</h2>:<h2>{time}</h2>}
+            <input value={inputValue} onFocus={onFocusHandler} onBlur={onBlurHandler} onChange={handleOnChange}/>
         </div>
     );
 }
